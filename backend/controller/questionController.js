acb6eccae37c061a8d20971f190333ca2a5dc4e9
Eyerusalem <jerrykinu2885@gmail.com>
2025-12-31 13:49:33 +0300

    added a query to fetch the questions

--- backend/controller/questionController.js
+++ backend/controller/questionController.js
@@ -12,7 +12,45 @@ async function postQuestion(req, res) {
 
 // Student 2 - Task B
 async function getAllQuestions(req, res) {
-  res.send("All questions logic coming soon");
+try {
+  // SQL query to get all questions with user information
+  const query = `
+            SELECT 
+                q.questionid,
+                q.title,
+                q.description,
+                q.tag,
+                q.id,
+                q.created_at,
+                u.userid,
+                u.username,
+                u.firstname,
+                u.lastname
+            FROM questions q
+            JOIN users u ON q.userid = u.userid
+            ORDER BY q.id DESC
+        `;
+  //destructure the result to get only the rows
+  const [questions] = await db.execute(query);
+
+  // Check if questions exist
+  if (questions.length === 0) {
+    return res.status(StatusCodes.NOT_FOUND).json({
+      error: true,
+      message: "No questions found",
+      data: [],
+    });
+  }
+
+  res.status(StatusCodes.OK).json({
+    error: false,
+    message: "Questions retrieved successfully",
+    data: questions,
+    count: questions.length,
+  });
+} catch (error) {
+  
+}
 }
 
 // Student 3 - Task C
