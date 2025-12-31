dd00c04a30e7e588e179926ee2c27f5d05f825e8
Seble-noli <seblewengel1227@gmail.com>
2025-12-30 21:58:33 -0800

    userregistercontroller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,7 +4,28 @@ const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
 
 async function register(req, res) {
-  // Student 1 will implement registration logic here
+  const{username, firstname,lastname,email,password} =req.body;
+  if(!email||!password|| !firstname || !lastname || !username){
+    return res.status(400).json({msg: "please provide all required fields"})
+  }
+  try{
+    const [user] =await dbConnection.query("select username,userid from users where username = ? or email =?",[username,email])
+    if(user.length>0){
+      return res.status(400).json({msg: "user already existed"})
+    }
+    if(password.length<=8){
+     return res.status(400).json({msg:"password must be at least 8 character"})
+    }
+    //encrypt the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedpassword = await bcrypt.hash(password,salt)
+    await dbConnection.query("INSERT INTO users (usrname,lastname,email,password) VALUES (?,?,?,?,?",[username,firstname,lastname,email,hashedpassword])
+
+    return res.status(StatusCodes.CREATED).json({msg:"something went wrong,try again later!"})
+  }  catch (error){
+       console.log(error.message)
+       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"somthing went wrong"})
+  }
   res.send("register logic");
 }
 
