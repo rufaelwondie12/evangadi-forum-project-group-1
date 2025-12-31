9cd0a4261dd6e2042b3609624900c6a7226663aa
Eyerusalem <jerrykinu2885@gmail.com>
2025-12-31 13:53:48 +0300

    added question route file using get method to fetch only authenticated users

--- backend/routes/questionRoute.js
+++ backend/routes/questionRoute.js
@@ -14,14 +14,14 @@ const {
 // 1. Post a question - [POST] /api/question/
 router.post("/postQuestion", authMiddleware, postQuestion);
 
-// 2. Get all questions - [GET] /api/question/
+// 2. Get all questions - [GET] /api/question/ method to fetch all questions only for authenticated users
 router.get("/getAllQuestions", authMiddleware, getAllQuestions);
 
 // 3. Get a single question - [GET] /api/question/:question_id
 router.get(
   "/getSingleQuestion/:question_id",
   authMiddleware,
-  getSingleQuestion,
+  getSingleQuestion
 );
 
 module.exports = router;
