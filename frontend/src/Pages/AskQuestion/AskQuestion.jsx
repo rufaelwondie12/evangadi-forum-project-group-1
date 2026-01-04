4abc13bfee3143a217c5cf227355373f3cf54e47
hana <wondiehana@gmail.com>
2026-01-04 20:58:43 +0300

    AskQuestion.jsx is added

--- frontend/src/Pages/AskQuestion/AskQuestion.jsx
+++ frontend/src/Pages/AskQuestion/AskQuestion.jsx
@@ -1,13 +1,9 @@
-import React from "react";
-
-import classes from "./AskQuestion.module.css";
+import QuestionForm from "../features/questions/QuestionForm";
 
 const AskQuestion = () => {
   return (
-    <div className={classes.ask_container}>
-      <h2 className={classes.title}>Ask a public question</h2>
-
-      <form className={classes.form}>{/* Students build the rest here */}</form>
+    <div style={{ maxWidth: "800px", margin: "40px auto" }}>
+      <QuestionForm />
     </div>
   );
 };
