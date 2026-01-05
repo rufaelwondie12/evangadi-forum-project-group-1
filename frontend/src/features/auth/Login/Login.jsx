79c6a15a6e0e93bb43c39ab6d789969e1c4f240d
yoseph2025 <yosephgashaye1@gmail.com>
2026-01-05 22:04:44 +0300

    auth/login

--- frontend/src/features/auth/Login/Login.jsx
+++ frontend/src/features/auth/Login/Login.jsx
@@ -1,25 +1,52 @@
-import React, { useState } from "react";
-import { useAuth } from "../../../context/AuthContext";
-import axiosBase from "../../../axios/axiosConfig";
+import React, { useRef } from "react";
+// import { useAuth } from "../../../context/AuthContext";
+import axiosBase from "../../../services/axiosConfig";
+import classes from "./Login.module.css";
 
 const Login = () => {
-  const { setUser } = useAuth();
+  // const { setUser } = useAuth();
+  const navigate = useNavigate();
+  const emailDom = useRef();
+  const passwordDom = useRef();
   // Sami/Yosi: Add your state for email and password here
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Sami/Yosi:
-    // 1. Call axiosBase.post("/user/login")
-    // 2. Save token to localStorage
-    // 3. Update state with setUser()
+    const emailValue = emailDom.current.value;
+    const passwordValue = passwordDom.current.value;
+
+    if (!emailValue || !passwordValue) {
+      alert("Please provide all required information");
+      return;
+    }
+
+    try {
+      const { data } = await axiosBase.post("/user/login", {
+        email: emailValue,
+        password: passwordValue,
+      });
+      alert("Login successfully.");
+      localStorage.setItem("token", data.token);
+      navigate("/");
+      console.log(data);
+    } catch (error) {
+      alert(error?.response?.data?.msg);
+      console.log(error.response.data);
+    }
   };
 
   return (
-    <section>
+    <section className={classes.container}>
       <h2>Login to your account</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className={classes.inputGroup}>
         {/* Add Email and Password inputs here */}
-        <button type="submit" className="orange_btn">
+        <input ref={emailDom} type="email" placeholder="Email" />
+
+        <input ref={passwordDom} type="password" placeholder="Password" />
+        <div className={classes.checkboxWrapper}>
+          <Link to="/terms">Forget password</Link>
+        </div>
+        <button type="submit" className={classes.button}>
           Login
         </button>
       </form>
--- frontend/src/features/auth/Register/Register.jsx
+++ frontend/src/features/auth/Register/Register.jsx
@@ -1,22 +1,72 @@
-import React, { useState } from "react";
-import axiosBase from "../../../axios/axiosConfig";
+import React, { useRef } from "react";
+import axiosBase from "../../../services/axiosConfig";
+import { useNavigate } from "react-router-dom";
+import classes from './Register.module.css'
 
 const Register = () => {
   // Sami/Yosi: Add state for username, firstname, lastname, email, password
+  const navigate = useNavigate();
+  const usernameDom = useRef();
+  const firstNameDom = useRef();
+  const lastNameDom = useRef();
+  const emailDom = useRef();
+  const passwordDom = useRef();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Sami/Yosi:
     // 1. Call axiosBase.post("/user/register")
+
     // 2. Handle success (navigate to login)
+    const usernameValue = usernameDom.current.value;
+    const firstnameValue = firstNameDom.current.value;
+    const lastnameValue = lastNameDom.current.value;
+    const emailValue = emailDom.current.value;
+    const passwordValue = passwordDom.current.value;
+
+    if (
+      !usernameValue ||
+      !firstnameValue ||
+      !lastnameValue ||
+      !emailValue ||
+      !passwordValue
+    ) {
+      alert("Please provide all required information");
+      return;
+    }
+
+    try {
+      await axiosBase.post("/user/register", {
+        username: usernameValue,
+        firstname: firstnameValue,
+        lastname: lastnameValue,
+        email: emailValue,
+        password: passwordValue,
+      });
+      alert("Registered successfully. Please login.");
+      navigate("/login");
+    } catch (error) {
+      alert("Something went wrong");
+      console.log(error.response);
+    }
   };
 
   return (
     <section>
       <h2>Join the network</h2>
+      <p>
+        Already have an account? <a href="/login">Login</a>
+      </p>
       <form onSubmit={handleSubmit}>
-        {/* Add all 5 registration inputs here */}
-        <button type="submit" className="orange_btn">
+        <input ref={usernameDom} type="text" placeholder="Username" />
+        <div className={classes["name-fields"]}>
+          <input ref={firstNameDom} type="text" placeholder="First Name" />
+          <input ref={lastNameDom} type="text" placeholder="Last Name" />
+        </div>
+        <input ref={emailDom} type="email" placeholder="Email" />
+        <input ref={passwordDom} type="password" placeholder="Password" />
+
+        <button type="submit" className={classes.button}>
           Agree and Join
         </button>
       </form>
