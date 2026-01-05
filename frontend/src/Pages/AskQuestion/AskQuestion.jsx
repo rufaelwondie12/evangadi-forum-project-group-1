0ed063c44306fd221d921bb5d484cc66bd554c5c
hana <wondiehana@gmail.com>
2026-01-05 17:00:45 +0300

    askquestion.jsx is added

--- frontend/src/Pages/AskQuestion/AskQuestion.jsx
+++ frontend/src/Pages/AskQuestion/AskQuestion.jsx
@@ -1,17 +1,6 @@
 import QuestionForm from "../../features/questions/QuestionForm/QuestionForm";
 
 const AskQuestion = () => {
-  // if (!localStorage.getItem("token")) {
-  //   localStorage.setItem(
-  //     "token",
-  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFhYSIsInVzZXJpZCI6NywiaWF0IjoxNzY3NTU2NzcwLCJleHAiOjE3Njc2NDMxNzB9.KcdejqDl5oqnGFtpDZmvI0w2ZXzjS0_n3y0Pqs_fkmQ"
-  //   );
-  //   localStorage.setItem(
-  //     "user",
-  //     JSON.stringify({ username: "testuser", userid: 7 })
-  //   );
-  // }
-
   return (
     <div style={{ maxWidth: "800px", margin: "40px auto" }}>
       <QuestionForm />
